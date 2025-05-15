d59078ba4a73e4a41a0255c545dd8a341f680a4c
Patrik <Muravski.patrik@gmail.com>
2025-05-15 11:34:14 +0300

    test for controller added

---
+++ MicroserviceCommand/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceCommand.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestConnection()
+        {
+            Console.WriteLine("Post in a COMMAND SERVICE");
+
+            return Ok("Test from command srvce");
+        }
+    }
+}

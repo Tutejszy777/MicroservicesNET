05ed4c9527ebbd49ef3ac96b2a858b4988da3326
Patrik <Muravski.patrik@gmail.com>
2025-05-29 22:42:08 +0300

    created platform class

---
+++ MicroserviceCommand/Models/Platform.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroserviceCommand.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+        public int ExternalId { get; set; }
+        public string Name { get; set; }
+    }
+}

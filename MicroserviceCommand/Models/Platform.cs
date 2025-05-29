4fd2b2d6a7f4672ab74f36fbbcf267ef87079be2
Patrik <Muravski.patrik@gmail.com>
2025-05-29 22:43:57 +0300

    added colection to a class

--- MicroserviceCommand/Models/Platform.cs
+++ MicroserviceCommand/Models/Platform.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public int ExternalId { get; set; }
         public string Name { get; set; }
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
     }
 }

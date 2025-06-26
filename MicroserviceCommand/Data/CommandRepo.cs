d36b578e514b318c4339ac4c3ea73eb401d198a3
Patrik <Muravski.patrik@gmail.com>
2025-06-26 22:08:07 +0300

    implemented class

--- MicroserviceCommand/Data/CommandRepo.cs
+++ MicroserviceCommand/Data/CommandRepo.cs
@@ -1,6 +1,68 @@
-﻿namespace MicroserviceCommand.Data
+﻿using MicroserviceCommand.Models;
+
+namespace MicroserviceCommand.Data
 {
-    public class CommandRepo
+    public class CommandRepo : ICommandRepo
     {
+        private readonly AppDbContext _context;
+
+        public CommandRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            _context.Commands.Add(command);
+        }
+
+        public void CreatePlatform(Platform plat)
+        {
+            if (plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+            _context.Platforms.Add(plat);
+        }
+
+        public bool ExternalPlatformExists(int externalPlatformId)
+        {
+            return _context.Platforms.Any(p => p.ExternalId == externalPlatformId);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId && c.Id == commandId)
+                .FirstOrDefault();
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name);
+        }
+
+        public bool PlaformExits(int platformId)
+        {
+            return _context.Platforms.Any(p => p.Id == platformId);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
     }
 }
